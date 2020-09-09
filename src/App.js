e802634d52ee5d12e8b26ded44c03c272ff73f80
Niraj Dewangan <niraj.dewangan1608@gmail.com>
2020-09-09 08:45:57 +0530

    ADD: Launch Date

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ import './App.css'
 
 function App() {
   
-  //const [apiData, setApiData] = useState({})
-  //const [error, setError] = useState(false)
   const [launches, setLaunches] = useState([])
   const [successfulLanding, setSuccessfulLanding] = useState(false)
-  //const [successfulLaunch, setSuccessfulLaunch] = useState(false)
+  const [apiUrl, setApiUrl] = useState(`https://api.spacexdata.com/v3/launches?limit=100`)
+  const [successfulLaunch, setSuccessfulLaunch] = useState(false)
 
-  const apiUrl = `https://api.spacexdata.com/v3/launches?limit=100`;  
   const getData = async(apiUrl) =>{
     const result = await Axios.get(apiUrl) 
-    //setApiData(result)
     setLaunches(result.data)
     return result;
   }
@@ -23,14 +20,19 @@ function App() {
 
   const onChangeSuccessfulLanding = (e) =>{
     setSuccessfulLanding(e)
-    console.log(successfulLanding)
-
+    setApiUrl(`https://api.spacexdata.com/v3/launches?limit=100`)
+    
   }
 
-  // const onChangeSuccessfulLaunch = (e) =>{
-  //   setSuccessfulLaunch(e)
+  const onChangeSuccessfulLaunch = (e) =>{
+    setSuccessfulLaunch(e)
+    setApiUrl(`https://api.spacexdata.com/v3/launches?limit=100`)
     
-  // }
+  }
+
+  const onChangeYear = (e) => {
+    setApiUrl(`https://api.spacexdata.com/v3/launches?limit=100&launch_year=${e}`)
+  }
   
   useEffect( () => {
     getData(apiUrl)
@@ -48,22 +50,36 @@ function App() {
                 <h6>Launch Date</h6>
                 <hr/>
                 <div className="text-center"> 
-                  <button className="btn btn-success m-2">2006</button>
-                  <button className="btn btn-success m-2">2007</button>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2006)}>2006</button>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2007)}>2007</button>
                   <br/>
-                  <button className="btn btn-success m-2">2006</button>
-                  <button className="btn btn-success m-2">2007</button>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2008)}>2008</button>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2009)}>2009</button>
                   <br/>
-                  <button className="btn btn-success m-2">2006</button>
-                  <button className="btn btn-success m-2">2007</button>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2010)}>2010</button>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2011)}>2011</button>
+                  <br/>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2012)}>2012</button>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2013)}>2013</button>
+                  <br/>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2014)}>2014</button>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2015)}>2015</button>
+                  <br/>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2016)}>2016</button>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2017)}>2017</button>
+                  <br/>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2018)}>2018</button>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2019)}>2019</button>
+                  <br/>
+                  <button className="btn btn-success m-2" onClick={() => onChangeYear(2020)}>2020</button>
                   <br/>
                 </div>
                 
                 <h6>Successful Launch</h6>
                 <hr/>
                 <div className="text-center"> 
-                  <button className="btn btn-success m-2">True</button>
-                  <button className="btn btn-success m-2">False</button>
+                  <button className="btn btn-success m-2" onClick={() => onChangeSuccessfulLaunch(true)}>True</button>
+                  <button className="btn btn-success m-2" onClick={() => onChangeSuccessfulLaunch(false)}>False</button>
                   <br/>
                 </div>
                 <h6>Successful Landing</h6>
